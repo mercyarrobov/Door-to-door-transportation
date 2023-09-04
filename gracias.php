b5d282b905d7122c1a95315abdebaf76c7e5afbb
mercyarrobov <myarrobo@espe.edu.ec>
2023-09-04 15:12:49 -0500

    public

---
+++ gracias.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gracias - Tu Empresa de Transporte Puerta a Puerta</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+    <header>
+        <h1>¡Gracias por Contactarnos!</h1>
+        <p>Te agradecemos por ponerte en contacto con nosotros. Hemos recibido tu mensaje y nos pondremos en contacto contigo lo antes posible.</p>
+    </header>
+
+    <section>
+        <h2>¿Qué Puedes Hacer a Continuación?</h2>
+        <p>Mientras tanto, si tienes alguna pregunta adicional o necesitas asistencia inmediata, no dudes en llamarnos al número de contacto proporcionado o enviarnos un correo electrónico a nuestra dirección de correo electrónico.</p>
+    </section>
+
+    <footer>
+        <p>Volver a la <a href="index.php">Página de Inicio</a></p>
+    </footer>
+</body>
+</html>
