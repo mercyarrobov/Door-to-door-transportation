958c1b7a7385c4b2735a48aba1ae2d63f4d16cee
mercyarrobov <myarrobo@espe.edu.ec>
2023-09-04 15:10:06 -0500

    public

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <title>Tu Empresa de Transporte Puerta a Puerta</title>
+</head>
+<body>
+    <header>
+        <h1>Bienvenido a Tu Empresa de Transporte Puerta a Puerta</h1>
+        <p>Ofrecemos servicios de transporte confiables y seguros.</p>
+        <a href="contacto.php">Solicita una cotización</a>
+    </header>
+
+    <section>
+        <h2>Nuestros Servicios</h2>
+        <ul>
+            <li>Transporte de mercancías puerta a puerta</li>
+            <li>Mudanzas locales y nacionales</li>
+            <li>Transporte de pasajeros</li>
+        </ul>
+    </section>
+
+    <section>
+        <h2>Por qué Elegirnos</h2>
+        <p>Somos una empresa comprometida con la satisfacción del cliente. Nuestro equipo de profesionales garantiza un servicio puntual y seguro.</p>
+    </section>
+
+    <footer>
+        <p>Contáctanos: <a href="contacto.php">Haz clic aquí</a></p>
+    </footer>
+</body>
+</html>
